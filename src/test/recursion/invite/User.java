c55e12b59407641f3b4a5bbd23b7eb29ec3bb49e
hong <mynamehongshaojia@gmail.com>
2022-07-30 18:13:50 +0800

    递归测试代码

---
+++ src/test/recursion/invite/User.java
@@ -0,0 +1,27 @@
+package test.recursion.invite;
+
+class User {
+    private Integer userId;//用户的id
+    private User inviteUser;//邀请用户的id
+
+    public User(Integer userId, User inviteUser) {
+        this.userId = userId;
+        this.inviteUser = inviteUser;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public User getInviteUser() {
+        return inviteUser;
+    }
+
+    public void setInviteUser(User inviteUser) {
+        this.inviteUser = inviteUser;
+    }
+}
